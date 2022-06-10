3a56d99dba3accbf2855d93ee48b860276537f4f
Anth is making softs <60937839+anthDev@users.noreply.github.com>
2022-06-10 13:35:04 +0200

    Update RiddleApi.php

--- app/controllers/API/RiddleApi.php
+++ app/controllers/API/RiddleApi.php
@@ -11,10 +11,12 @@ class RiddleApi extends Controller
 {
     function index()
     {
-        echo "Hello API";
-
         $user = new User($this->getDB());
         $users = $user->all();
-        
+        $data = serialize($users);
+        $output = json_encode($data);
+        header('Content-Type: application/json; charset=utf-8');
+        header('Content-Lenght: ' . strlen($output));
+        echo $output;
     }
 }
