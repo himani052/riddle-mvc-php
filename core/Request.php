08f8c4e5a6063bb11c2c9b17be08032ac16d6f0b
unknown <thomas.es13@gmail.com>
2022-06-21 17:01:58 +0200

    connexion bdd

--- core/Request.php
+++ core/Request.php
@@ -86,7 +86,8 @@ class Request
         if (is_string($this->action)) {
             $action = explode('@', $this->action);
             $controller = $action[0];
-            $controller = new $controller(new DBConnection('test', 'localhost', 'root', ''));
+            //$controller = new $controller(new DBConnection('test', 'localhost', 'root', ''));
+            $controller = new $controller(new DBConnection('riddle_test', 'mysql-riddle.alwaysdata.net', 'riddle', 'azert123&'));
             $method = $action[1];
             call_user_func_array([$controller, $method], $this->params);
             //return isset($this->params) ? $controller->$method(implode($this->params)) : $controller->$method(); isset($this->params) ? $controller->method(implode($this->params)) : $controller->method();
@@ -101,7 +102,8 @@ class Request
         if (is_string($this->action)) {
             $action = explode('@', $this->action);
             $controller = $action[0];
-            $controller = new $controller(new DBConnection('test', 'localhost', 'root', ''));
+            //$controller = new $controller(new DBConnection('test', 'localhost', 'root', ''));
+            $controller = new $controller(new DBConnection('riddle_test', 'mysql-riddle.alwaysdata.net', 'riddle', 'azert123&'));
             $method = $action[1];
             //on ajoute l'object request dans le tableau de paramètres
             array_unshift($this->params, $this->request);
